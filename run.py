f483994824d2b689b97b152cf4444e2916147960
Gabi Delcea <vioriiiee@gmail.com>
2024-12-22 13:43:09 +0200

    .

--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, ssl_context=('/etc/letsencrypt/live/gabi-final-project-devops.online/fullchain.pem', '/etc/letsencrypt/live/gabi-final-project-devops.online/privkey.pem'))
+
